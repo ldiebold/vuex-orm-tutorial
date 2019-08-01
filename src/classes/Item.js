cc3ac6940629b862cf40b75ce0d642ccfed2632f
ldiebold <luke@ldiebold.com>
2019-08-01 10:08:35 +1000

    remove api calls

--- src/classes/Item.js
+++ src/classes/Item.js
@@ -4,24 +4,6 @@ import List from './List'
 export default class Item extends Model {
   static entity = 'items'
 
-  static $create(form) {
-    return axios.post('/api/items', form)
-    .then(response => {
-      Item.insert(response.data)
-      return response
-    })
-  }
-
-  get $update() {
-    return (form) => {
-      return axios.patch('/api/items', form)
-      .then(response => {
-        Item.update({ data: response.data, where: response.id })
-        return response
-      })
-    }
-  }
-
   static fields() {
     return {
       id: this.increment(),
