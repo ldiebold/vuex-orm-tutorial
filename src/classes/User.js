7a11718fdafdc1a62021b0b27323938895c3af26
ldiebold <luke@ldiebold.com>
2019-09-27 10:59:27 +1000

    end of accessors and mutators video

--- src/classes/User.js
+++ src/classes/User.js
@@ -5,15 +5,34 @@ import Item from './Item'
 import Role from './Role'
 import RoleUser from './RoleUser'
 import Image from './Image'
+import moment from 'moment'
 
 export default class User extends Model {
   static entity = 'users'
 
+  get full_name() {
+    return `${this.first_name} ${this.last_name}`
+  }
+
+  static mutators() {
+    return {
+      last_name(value) {
+        return value.toUpperCase()
+      },
+
+      date_born(value) {
+        return moment(value)
+      }
+    }
+  }
+
   static fields() {
     return {
       id: this.increment(),
-      name: this.attr(''),
+      first_name: this.attr(''),
+      last_name: this.attr(''),
       email: this.attr(''),
+      date_born: this.attr(null),
       // relationships
       profile: this.hasOne(Profile, 'user_id'),
       lists: this.hasMany(List, 'user_id'),
---
+++ src/components/AccessorsAndMutators.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    {{ user.date_born.add(5, 'year') }}
+  </div>
+</template>
+
+<script>
+import User from '../classes/User'
+
+export default {
+  props: {
+
+  },
+
+  mounted() {
+    User.insert({
+      data: {
+        id: 28,
+        date_born: '1991-04-27T14:00:00.000Z',
+        first_name: 'Luke',
+        last_name: 'Diebold',
+      }
+    })
+  },
+
+  components: {
+
+  },
+
+  computed: {
+    user() {
+      return User.find(28)
+    }
+  },
+
+  methods: {
+    
+  },
+
+  data() {
+    return {
+      
+    }
+  },
+}
+
+</script>
